221c1018ef27978eecab704c58623de4be548136
Marcelo <marcaldo@gmail.com>
2024-03-31 11:01:02 -0500

    wip

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,61 +1,43 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
-using Avalonia.Media.Imaging;
 using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using System;
 using System.IO;
-using Tmds.DBus.Protocol;
 
 namespace TimeReflector
 {
     public partial class MainWindow : Window
     {
-        //private Image? _backgroundImage;
-        //private TextBlock? _clickableLabel!;
-        public Image BkgImage = new();
-        private ImageBrush _backgroundBrush;
+        private ImageBrush _backgroundBrush = new();
         public MainWindow()
         {
             InitializeComponent();
-
             DisplayImages();
-
-            LoadBackgroundImage();
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-            //_backgroundImage = this.FindControl<Image>("backgroundImage");
-            //_clickableLabel = this.FindControl<TextBlock>("clickableLabel");
-        }
-
-        private void LoadBackgroundImage()
-        {
-            var backgroundImage = new Bitmap("C:/Users/marca/source/repos/timereflector/Images/superior.jpg");
-            _backgroundBrush = new ImageBrush(backgroundImage);
-            Resources["BackgroundBrush"] = _backgroundBrush;
         }
 
         private void DisplayImages()
         {
-            DisplayImageFromFile("superior.jpg");
+            LoadBackgroundImage("superior.jpg");
         }
 
-        private void DisplayImageFromFile(string fileName)
+        private void LoadBackgroundImage(string fileName)
         {
             string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", fileName);
-            // Create an Image control
-            BkgImage = new Image
-            {
-                Source = new Avalonia.Media.Imaging.Bitmap(imagePath)
-            };
+
+            var backgroundImage = new Bitmap(imagePath);
+            _backgroundBrush = new ImageBrush(backgroundImage);
+            Resources["BackgroundBrush"] = _backgroundBrush;
         }
 
         private void Label_OnPointerPressed(object sender, Avalonia.Input.PointerPressedEventArgs e)
         {
-            Console.WriteLine("Label clicked!");
+            //Console.WriteLine("Label clicked!");
             // Do different actions here
         }
     }
